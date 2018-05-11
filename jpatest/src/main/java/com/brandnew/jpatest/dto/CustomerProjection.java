b7e3af8f68d3a40fdbdd0238e9dc0f77d6566365
zmh <623299342@qq.com>
2018-05-11 17:13:12 +0800

    done

--- jpatest/src/main/java/com/brandnew/jpatest/dto/CustomerProjection.java
+++ jpatest/src/main/java/com/brandnew/jpatest/dto/CustomerProjection.java
@@ -2,6 +2,9 @@ package com.brandnew.jpatest.dto;
 
 import org.springframework.beans.factory.annotation.Value;
 
+/**
+ * projection 投影
+ */
 public interface CustomerProjection {
     @Value("#{target.firstName+' '+target.lastname}")
     String getFullName();

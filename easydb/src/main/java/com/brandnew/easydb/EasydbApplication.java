f71ad2707d251887e61f495a7d1b63ae5701a2ed
zmh <623299342@qq.com>
2018-06-03 19:54:24 +0800

    remind me

--- easydb/src/main/java/com/brandnew/easydb/EasydbApplication.java
+++ easydb/src/main/java/com/brandnew/easydb/EasydbApplication.java
@@ -3,6 +3,10 @@ package com.brandnew.easydb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *  使用内存数据库h2 + jpa 方便快速开发，但是项目的初始化数据还是需要从data.sql或者schema.sql读入
+ * @createDate 2018/6/3
+ */
 @SpringBootApplication
 public class EasydbApplication {
 

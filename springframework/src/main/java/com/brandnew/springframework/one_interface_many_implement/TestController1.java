2e6f6cb3e518b6f3e573656f9aa6e34304a28b8c
zhaom <zhaomenghui93@163.com>
2018-07-01 19:46:00 +0800

    little more

--- springframework/src/main/java/com/brandnew/springframework/one_interface_many_implement/TestController1.java
+++ springframework/src/main/java/com/brandnew/springframework/one_interface_many_implement/TestController1.java
@@ -8,9 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController1 {
     @Autowired
     private IOneInterface oneInterface1;
-
+    @Autowired
+    private IOneInterface oneInterface2;
     @GetMapping("test1")
     public String test1() {
         return oneInterface1.intro();
+    }
+
+    @GetMapping("test2")
+    public String test2() {
+        return oneInterface2.intro();
     }
 }

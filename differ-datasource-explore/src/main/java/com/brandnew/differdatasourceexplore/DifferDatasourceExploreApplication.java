ef2e5cee4da241c08c39df9e38e1c6e1ed89320e
zmh_new <zhaomenghui93@163.com>
2018-11-21 23:26:22 +0800

    init

---
+++ differ-datasource-explore/src/main/java/com/brandnew/differdatasourceexplore/DifferDatasourceExploreApplication.java
@@ -0,0 +1,12 @@
+package com.brandnew.differdatasourceexplore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DifferDatasourceExploreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DifferDatasourceExploreApplication.class, args);
+    }
+}
---
+++ differ-datasource-explore/src/test/java/com/brandnew/differdatasourceexplore/DifferDatasourceExploreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.brandnew.differdatasourceexplore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DifferDatasourceExploreApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

1c96773bf1b6ab4b1b45add7bf9f2ab5cc170fc3
zmh2 <zmh.infomation@gmail.com>
2019-03-28 14:15:09 +0800

    dynamic proxy inside InvocationHandler

--- design-pattern/src/dynamic_proxy/App.java
+++ design-pattern/src/dynamic_proxy/App.java
@@ -16,6 +16,7 @@ public class App {
 
     public static void main(String[] args) {
         ICup cup      = new Cup();
+        //下面这句可以写在 InvocationHandler的实现类中,简化代码
         ICup cupProxy = (ICup) Proxy.newProxyInstance(ICup.class.getClassLoader(), cup.getClass().getInterfaces(), new CupProxy(cup));
         cupProxy.drink();
     }
